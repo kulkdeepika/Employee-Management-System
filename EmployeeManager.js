118eb17dd1b83bdfb00404a09060b5c98f090837
Deepika <kulkdeepika@gmail.com>
2020-03-01 22:43:03 -0800

    added code

--- EmployeeManager.js
+++ EmployeeManager.js
@@ -1,14 +1,15 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
-const printTable = require("console.table");
 const figlet = require("figlet");
 const boxen = require("boxen");
+const printTable = require("console.table");
+const queries = require("./queries.js");
 
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Abhiiappi-2311",
+    password: "password123",
     database:"EMPLOYEE_MANAGEMENT_SYSTEM"
 });
 
@@ -20,64 +21,24 @@ connection.connect(function(err){
     welcomeUser();
 })
 
-function promptUser(){
-    inquirer.prompt({
-        name: "action",
-        type: "list",
-        message: "\nWhat would you like to do?",
-        choices: [
-            "View all Employees",
-            "Add Employee",
-            "Add Department",
-            "Add Role",
-            "Update Employee Role"
-        ]
-    }).then(function(answer){
-        switch(answer.action){
-            case "View all Employees":
-                showAllEmployees();
-            break;
+// const showAllEmployees = function (){
+//     var query = connection.query(
+//         "SELECT EMPLOYEE.id,EMPLOYEE.first_name,EMPLOYEE.last_name, ROLE.title, ROLE.salary, DEPARTMENT.name as department, concat(e.first_name, ' ' ,e.last_name) as manager from EMPLOYEE join ROLE ON EMPLOYEE.role_id = ROLE.id join DEPARTMENT ON ROLE.department_id = DEPARTMENT.id left outer join EMPLOYEE e ON EMPLOYEE.manager_id = e.id;",
+//         (err, result) => {
+//             if(err){
+//                 throw err;
+//             }
 
-            case "Add Employee":
-                addEmployee();
-            break;
+//             console.log("\n" + printTable.getTable(result));
+//         }
+//     )
+// }
 
-            case "Add Department":
-                addDepartment();
-            break;
-
-            case "Add Role":
-                addRole();
-            break;
-
-            case "Update Employee Role":
-                updateEmpRole();
-            break;
-
-            default:
-                quit();
-        }
-    })
-} 
-
-function showAllEmployees(){
-    var query = connection.query(
-        "SELECT EMPLOYEE.id,EMPLOYEE.first_name,EMPLOYEE.last_name, ROLE.title, ROLE.salary, DEPARTMENT.name as department, concat(e.first_name, ' ' ,e.last_name) as manager from EMPLOYEE join ROLE ON EMPLOYEE.role_id = ROLE.id join DEPARTMENT ON ROLE.department_id = DEPARTMENT.id left outer join EMPLOYEE e ON EMPLOYEE.manager_id = e.id;",
-        (err, result) => {
-            if(err){
-                throw err;
-            }
-
-            console.log("\n" + printTable.getTable(result));
-        }
-    )
-}
-
-function addEmployee(){}
-function addDepartment(){}
-function addRole(){}
-function updateEmpRole(){}
-function quit(){}
+// function addEmployee(){}
+// function addDepartment(){}
+// function addRole(){}
+// function updateEmpRole(){}
+// function quit(){}
 
 function welcomeUser(){
     figlet("Employee\n\n\nManagement\n\n\nSystem", function(err,data){
@@ -86,6 +47,6 @@ function welcomeUser(){
             throw err;
         }
         console.log(boxen(data, {padding:1, borderStyle: 'bold', backgroundColor: 'gray'}));
-        promptUser();
+        queries.promptUser();
     })
 }
